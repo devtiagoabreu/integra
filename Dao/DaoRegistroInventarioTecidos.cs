358123adf870a70993c0834e5226b86eabaedfc3
Tiago de Abreu <devtiagoabreu@gmail.com>
2021-03-29 16:34:59 -0300

    Criando Objeto DaoRegistroInventarioTecidos

---
+++ Dao/DaoRegistroInventarioTecidos.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dao
+{
+    public class DaoRegistroInventarioTecidos
+    {
+        public string ProdutoCodigo { get; set; }
+        public string ProdutoDescricao { get; set; }
+        public string NumeroRolo { get; set; }
+        public string NumeroPeca { get; set; }
+        public string Situacao { get; set; }
+        public string Cor { get; set; }
+        public string CorDescricao { get; set; }
+        public string Desenho { get; set; }
+        public string Variante { get; set; }
+        public decimal Metros { get; set; }
+        public decimal Peso { get; set; }
+        public decimal CustoMetro { get; set; }
+        public decimal CustoMetroOutros { get; set; }
+        public decimal CustoTotal { get; set; }
+        public string DataSaida { get; set; }
+
+    }
+}
